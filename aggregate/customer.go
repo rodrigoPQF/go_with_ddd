61c4036a1e0c94fdf0c68a6e60c75ed889cba2bc
rodrigoPQF <rodrigopr404@gmail.com>
2023-07-02 20:24:42 -0300

    🚧 Customers: Aggregate

---
+++ aggregate/customer.go
@@ -0,0 +1,36 @@
+package aggregate
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/rodrigoPQF/go_with_ddd/entity"
+	"github.com/rodrigoPQF/go_with_ddd/valueobject"
+)
+
+var (
+	ErrInvalidPerson = errors.New("a customer has to have a valid name")
+)
+
+type Customer struct {
+	person       *entity.Person
+	products     []*entity.Item
+	transactions []valueobject.Transaction
+}
+
+func NewCustomer(name string) (Customer, error) {
+	if name == "" {
+		return Customer{}, ErrInvalidPerson
+	}
+
+	person := &entity.Person{
+		Name: name,
+		ID:   uuid.New(),
+	}
+
+	return Customer{
+		person:       person,
+		products:     make([]*entity.Item, 0),
+		transactions: make([]valueobject.Transaction, 0),
+	}, nil
+}
---
+++ aggregate/customer_test.go
@@ -0,0 +1,40 @@
+package aggregate_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/rodrigoPQF/go_with_ddd/aggregate"
+)
+
+func TestCustomer_NewCustomer(t *testing.T) {
+	type testCase struct {
+		test        string
+		name        string
+		expectedErr error
+	}
+
+	testCases := []testCase{
+		{
+			test:        "Empty name validation",
+			name:        "",
+			expectedErr: aggregate.ErrInvalidPerson,
+		},
+		{
+			test:        "Valid name",
+			name:        "Percy Bolmer",
+			expectedErr: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.test, func(t *testing.T) {
+			_, err := aggregate.NewCustomer(tc.name)
+
+			if !errors.Is(err, tc.expectedErr) {
+				t.Errorf("expected erro %v, got %v", tc.expectedErr, err)
+			}
+		})
+	}
+
+}
